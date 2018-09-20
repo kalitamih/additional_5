b68afb6f94619f0f220b434155936d234dd7a663
Kalita Mikhail <mkalita1991@gmail.com>
2018-09-20 18:25:22 +0300

    6th

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var counter = 1; 
+  
+  var brackets = []; 
+
+for (i=0; i < bracketsConfig.length; i++) 
+{ 
+  brackets[i] = bracketsConfig[i][0] + bracketsConfig[i][1]; 
+} 
+
+
+while (counter && str.length) 
+
+{ 
+
+  counter = 0; 
+
+ for (i=0; i < brackets.length; i++) 
+  { 
+   if ( str.indexOf(brackets[i]) != -1 )  { str = str.substring( 0, str.indexOf(brackets[i])) + str.substring( str.indexOf(brackets[i]) + 2 ); counter = 1; }; 
+  } 
+} 
+
+
+if (str.length) return false; 
+else return true;
+
+ 
 }
